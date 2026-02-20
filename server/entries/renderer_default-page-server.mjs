92eca70950a9fc555ad22a1cf329b2404e8a9af5
shockandco-dev <shockandco@gmail.com>
2026-02-20 18:22:46 +0000

    Deploying to gh-pages from @ shockandco-dev/Simtope-Feb-2026@ca0f4ba856e731953a5b31b29fc6d216d06fa3ef 🚀

---
+++ server/entries/renderer_default-page-server.mjs
@@ -0,0 +1,39 @@
+import { jsx } from "react/jsx-runtime";
+import ReactDOMServer from "react-dom/server";
+import React from "react";
+import { dangerouslySkipEscape, escapeInject } from "vite-plugin-ssr/server";
+const Context = React.createContext(null);
+function PageContextProvider({ pageContext, children }) {
+  return /* @__PURE__ */ jsx(Context.Provider, { value: pageContext, children });
+}
+function PageShell({ children, pageContext }) {
+  return /* @__PURE__ */ jsx(React.StrictMode, { children: /* @__PURE__ */ jsx(PageContextProvider, { pageContext, children }) });
+}
+async function render(pageContext) {
+  const { Page, pageProps } = pageContext;
+  const pageHtml = ReactDOMServer.renderToString(
+    /* @__PURE__ */ jsx(PageShell, { pageContext, children: /* @__PURE__ */ jsx(Page, { ...pageProps }) })
+  );
+  const { documentProps } = pageContext.exports;
+  const title = documentProps && documentProps.title || "Vite SSR app";
+  const desc = documentProps && documentProps.description || "App using Vite + vite-plugin-ssr";
+  const documentHtml = escapeInject`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="${desc}" />
+        <title>${title}</title>
+      </head>
+      <body>
+        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
+      </body>
+    </html>`;
+  return {
+    documentHtml,
+    pageContext: {}
+  };
+}
+export {
+  render
+};
