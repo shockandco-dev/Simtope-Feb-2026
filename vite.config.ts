262c1412ae42d1b0b29d17d3d3ea8c3dafe8c916
shockandco <shockandco@gmail.com>
2026-02-20 18:56:53 +0000

    Configure correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
+  base: '/Simtope-Feb-2026/',
   plugins: [react(), ssr()],
 });
